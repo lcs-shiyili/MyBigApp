dbc66ec24c6d64ea7dcbde8f167216b39b0b12c1
Shiyi Li <sli@lcs.on.ca>
2026-03-02 16:47:31 -0500

    Finished Unfinished View

--- StudyFlow/UnfinishedView.swift
+++ StudyFlow/UnfinishedView.swift
@@ -12,22 +12,33 @@ struct UnfinishedView: View {
     var body: some View {
         NavigationStack{
             VStack{
+                
+                Text("Study Flow")
+                    .font(.system(size: 35, weight: .bold))
+                    .padding(.top)
                
                 HStack(alignment: .center){
                     Text("Tasks")
                         .font(.headline)
+                    
+                    Spacer()
+                    
                     Text("Due date")
                         .font(.headline)
                 }
-                .background(Color.gray)
-                
-                
-                
+                .padding(.vertical, 10)
+                .padding(.horizontal, 20)
+                .background(Color.gray.opacity(0.35))
+                .clipShape(RoundedRectangle(cornerRadius: 18))
+                .padding(.top, 18)
+                .padding(.horizontal, 22)
+
                 .padding()
                 
-                VStack(alignment: .leading){
+                VStack(spacing: 18){
                     HStack{
                         Text("English essay")
+                        Spacer()
                         Text("Dec 18, 2025")
                     }
                     
@@ -35,15 +46,21 @@ struct UnfinishedView: View {
                     
                     HStack{
                         Text("Investment plan")
+                        Spacer()
                         Text("Dec 1, 2025")
                     }
                 }
                 
+                .padding(.top, 18)
+                .padding(.horizontal, 22)
+                
                 Spacer()
             }
           
+            .padding()
             
-            .navigationTitle("Study Flow")
+            
+            
             
             .toolbar {
                 
